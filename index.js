2acacbf5d8ecc358ccb320f7edbc9fffd467f438
unknown <skyredtea>
2023-05-19 21:37:27 -0400

    Add new files.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-1
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/files', (req, res) => {
+    // Handle the logic to retrieve files from GitHub API here
+  });
+
+app.get('/files', (req, res) => {
+  axios
+    .get('https://api.github.com/repos/your-username/your-repo/contents')
+    .then(response => {
+      // Handle the response from the GitHub API here
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Landing Page');
+  });  
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
