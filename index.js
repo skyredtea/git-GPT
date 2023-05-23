c0ff0df40d371fd1061418af2e08c835ab954a62
skyredtea <skyredtea@gmail.com>
2023-05-23 01:54:51 -0400

    Update

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
-const express = require('express');
-const axios = require('axios');
+const fs = require('fs');
+const fetch = require('isomorphic-fetch');
+const { Octokit } = require('@octokit/rest');
 
-const app = express();
-const port = 3000;
+const configData = fs.readFileSync('config.json');
+const config = JSON.parse(configData);
 
-app.get('/files', (req, res) => {
-    // Handle the logic to retrieve files from GitHub API here
-  });
+const { Configuration, OpenAIApi } = require('openai');
 
-app.get('/files', (req, res) => {
-  axios
-    .get('https://api.github.com/repos/your-username/your-repo/contents')
-    .then(response => {
-      // Handle the response from the GitHub API here
-      res.json(response.data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'An error occurred' });
-    });
+const configuration = new Configuration({
+  organization: config.openaiOrg,
+  apiKey: config.openaiAuth,
 });
 
-app.get('/', (req, res) => {
-    res.send('Landing Page');
-  });  
+const openai = new OpenAIApi(configuration);
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+async function getChatGPTResponse(messages) {
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    max_tokens: 750,
   });
+  const generatedText = response.data.choices[0].message.content.trim();
+  const totalTokens = response.data.usage.total_tokens;
+  console.log(`Total tokens used: ${totalTokens}`);
+  return generatedText;
+}
+
+async function getRepoContents(owner, repo, path) {
+  const octokit = new Octokit({
+    auth: config.gitAuth,
+  });
+  const response = await octokit.repos.getContent({
+    owner: owner,
+    repo: repo,
+    path: path,
+  });
+
+  if (response.status !== 200) {
+    throw new Error('Failed to fetch repository contents');
+  }
+
+  return response.data;
+}
+
+async function fetchGitHubRepoContents(owner, repo, path) {
+    const contents = await getRepoContents(owner, repo, path);
+    const fileContent = contents.content; // Get the base64-encoded content
+  
+    const decodedContent = Buffer.from(fileContent, 'base64').toString('utf-8');
+
+    return decodedContent;
+}
+
+async function getInput(question) {
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+async function chatLoop() {
+  let conversation = [
+    { role: 'system', content: 'You are a helpful assistant.' },
+  ];
+
+  const repoOwner = config.gitOwner;
+  const repoName = config.gitRepo;
+  const repoPath = config.gitPath;
+  const repoContents = await fetchGitHubRepoContents(repoOwner, repoName, repoPath);
+  conversation.push({ role: 'assistant', content: repoContents });
+
+  while (true) {
+    const prompt = await getInput('User: ');
+    if (prompt.toLowerCase() === '/quit') {
+      console.log('Goodbye!');
+      break;
+    }
+    conversation.push({ role: 'user', content: prompt });
+    const response = await getChatGPTResponse(conversation);
+    console.log('ChatGPT: ' + response);
+    conversation.push({ role: 'assistant', content: response });
+  }
+}
+
+chatLoop();
