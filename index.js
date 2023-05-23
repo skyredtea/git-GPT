a89309436d9b0ba4ae82dcfa18f21cb987e3ac43
skyredtea <skyredtea@gmail.com>
2023-05-23 06:03:16 -0400

    v1.1.6 reformat (again)

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const fs = require('fs');
-const { Configuration, OpenAIApi } = require('openai');
-const { Octokit } = require('@octokit/rest');
+const {
+  Configuration,
+  OpenAIApi
+} = require('openai');
+const {
+  Octokit
+} = require('@octokit/rest');
 
 const app = express();
 const port = 3000;
@@ -10,12 +15,12 @@ const configData = fs.readFileSync('config.json');
 const config = JSON.parse(configData);
 
 const octokit = new Octokit({
-    auth: config.gitAuth,
+  auth: config.gitAuth,
 });
 
 const configuration = new Configuration({
-    organization: config.openaiOrg,
-    apiKey: config.openaiAuth,
+  organization: config.openaiOrg,
+  apiKey: config.openaiAuth,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -25,93 +30,96 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'));
 
 async function fetchGitHubRepoContents(octokit) {
-    try {
-        const response = await octokit.request('GET /repos/{owner}/{repo}/contents', {
-            owner: config.gitOwner,
-            repo: config.gitRepo,
+  try {
+    const response = await octokit.request('GET /repos/{owner}/{repo}/contents', {
+      owner: config.gitOwner,
+      repo: config.gitRepo,
+    });
+
+    const files = await Promise.all(
+      response.data
+      .filter(file => file.type === 'file')
+      .map(async (file) => {
+        const contentResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+          owner: config.gitOwner,
+          repo: config.gitRepo,
+          path: file.path,
         });
 
-        const files = await Promise.all(
-            response.data
-            .filter(file => file.type === 'file')
-            .map(async (file) => {
-                const contentResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-                    owner: config.gitOwner,
-                    repo: config.gitRepo,
-                    path: file.path,
-                });
+        let content = '';
+        if (contentResponse.data.content) {
+          content = Buffer.from(contentResponse.data.content, 'base64').toString();
+        }
 
-                let content = '';
-                if (contentResponse.data.content) {
-                    content = Buffer.from(contentResponse.data.content, 'base64').toString();
-                }
-
-                return {
-                    name: file.name,
-                    content,
-                };
-            })
-        );
-        return files;
-    } catch (error) {
-        throw new Error('Error fetching GitHub repository contents: ' + error.message);
-    }
+        return {
+          name: file.name,
+          content,
+        };
+      })
+    );
+    return files;
+  } catch (error) {
+    throw new Error('Error fetching GitHub repository contents: ' + error.message);
+  }
 };
 
 async function getChatGPTResponse(messages, maxTokens) {
-    const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        max_tokens: maxTokens,
-    });
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    max_tokens: maxTokens,
+  });
 
-    const generatedText = response.data.choices[0].message.content.trim();
-    const tokensUsed = response.data.usage.total_tokens;
+  const generatedText = response.data.choices[0].message.content.trim();
+  const tokensUsed = response.data.usage.total_tokens;
 
-    return {
-        generatedText,
-        tokensUsed
-    };
+  return {
+    generatedText,
+    tokensUsed
+  };
 }
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/files', async (req, res) => {
-    try {
-        const files = await fetchGitHubRepoContents(octokit);
-        res.json({
-            files
-        });
-    } catch (error) {
-        console.error('Error fetching GitHub repository contents:', error);
-        res.status(500).json({
-            message: 'Error fetching repository contents'
-        });
-    }
+  try {
+    const files = await fetchGitHubRepoContents(octokit);
+    res.json({
+      files
+    });
+  } catch (error) {
+    console.error('Error fetching GitHub repository contents:', error);
+    res.status(500).json({
+      message: 'Error fetching repository contents'
+    });
+  }
 });
 
 app.post('/chat', async (req, res) => {
-    const conversation = req.body.message;
-    const maxTokens = req.body.max_tokens;
-    
-    try {
-        const { generatedText, tokensUsed } = await getChatGPTResponse(conversation, maxTokens);
-        res.json({
-            generatedText,
-            tokensUsed
-        });
-    } catch (error) {
-        console.error('Error in chat completion:', error);
-        res.status(500).json({
-            message: 'Error in chat completion'
-        });
-    }
+  const conversation = req.body.message;
+  const maxTokens = req.body.max_tokens;
+
+  try {
+    const {
+      generatedText,
+      tokensUsed
+    } = await getChatGPTResponse(conversation, maxTokens);
+    res.json({
+      generatedText,
+      tokensUsed
+    });
+  } catch (error) {
+    console.error('Error in chat completion:', error);
+    res.status(500).json({
+      message: 'Error in chat completion'
+    });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
 
 console.log(`node index.js is loaded.`);
